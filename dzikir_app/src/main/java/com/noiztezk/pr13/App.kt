c4a68caa0d9856d94d5af82652d7bae1f924a0f9
noiz354 <noiz354@gmail.com>
2018-11-22 16:19:52 +0700

    upgrade to latest version.

--- dzikir_app/src/main/java/com/noiztezk/pr13/App.kt
+++ dzikir_app/src/main/java/com/noiztezk/pr13/App.kt
@@ -5,6 +5,10 @@ import com.facebook.stetho.Stetho
 import com.raizlabs.android.dbflow.config.FlowConfig
 import com.raizlabs.android.dbflow.config.FlowManager
 import net.danlew.android.joda.JodaTimeAndroid
+import com.facebook.stetho.dumpapp.DumperPlugin
+import com.facebook.stetho.DumperPluginsProvider
+
+
 
 /**
  * Created by normansyahputa on 9/29/17.
